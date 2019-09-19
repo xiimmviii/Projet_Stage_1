4aca356c9fae0525e34bf3667d31d79f9c2dbf9b
Marine <marinepelletier@me.com>
2019-09-19 15:54:39 +0200

    Realisations

---
+++ realisations.php
@@ -0,0 +1,77 @@
+<?php
+require('header.php');
+?>
+
+<section class="mt-5 mb-5">
+    <div class="row justify-content-center">
+
+        <div class="col-md-2">
+            <img class="img-thumbnail" data-toggle="modal" data-target="#Modal1" src="https://image.jimcdn.com/app/cms/image/transf/none/path/se31ffe0c7aeb19f5/backgroundarea/ib25972f65a52afcf/version/1509638453/image.jpg">
+
+
+
+
+            <div class="modal fade" id="Modal1" tabindex="-1" role="dialog" aria-labelledby="Modal1Label" aria-hidden="true">
+                <div class="modal-dialog col-10" role="document">
+                    <div class="modal-content">
+                        <img src="https://image.jimcdn.com/app/cms/image/transf/none/path/se31ffe0c7aeb19f5/backgroundarea/ib25972f65a52afcf/version/1509638453/image.jpg" class="img-fluid" />
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-2">
+            <img class="img-thumbnail" data-toggle="modal" data-target="#exampleModal" src="https://www.stickpng.com/assets/images/58482d7fcef1014c0b5e4a5a.png">
+
+
+
+
+            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <img src="https://www.stickpng.com/assets/images/58482d7fcef1014c0b5e4a5a.png" class="img-fluid col-10" />
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-2">
+            <img class="img-thumbnail" data-toggle="modal" data-target="#exampleModal" src="https://www.stickpng.com/assets/images/58482d7fcef1014c0b5e4a5a.png">
+
+
+
+
+            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <img src="https://www.stickpng.com/assets/images/58482d7fcef1014c0b5e4a5a.png" class="img-fluid col-10" />
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-2">
+            <img class="img-thumbnail" data-toggle="modal" data-target="#exampleModal" src="https://www.stickpng.com/assets/images/58482d7fcef1014c0b5e4a5a.png">
+
+
+
+
+            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <img src="https://www.stickpng.com/assets/images/58482d7fcef1014c0b5e4a5a.png" class="img-fluid col-10" />
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+</div>
+
+</section>
+<?php
+require('footer.php');
+?>
