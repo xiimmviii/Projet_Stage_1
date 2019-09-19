5ad38a0a5737bde18632e8e296f1d72856d742af
Marine <marinepelletier@me.com>
2019-09-19 11:28:02 +0200

    Contenu UTD

--- index.php
+++ index.php
@@ -3,6 +3,12 @@ require('header.php');
 ?>
 
 <main>
+    <div class="row">
+        <div class="col-12 text-center index-img-titre p-4">EFFET DE TITRE</div>
+    </div>
+    <div class="row">
+        <div class="col-md-4 offset-6"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto alias rem error ipsa iste temporibus id facilis illum optio dolor?</div>
+    </div>
 </main>
 
 <section>
