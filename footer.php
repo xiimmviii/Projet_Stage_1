2b368d4c5f8f4ed07529bbfed94d133286576d41
Marine <marinepelletier@me.com>
2019-09-19 09:44:51 +0200

    BASE - 1

---
+++ footer.php
@@ -0,0 +1,7 @@
+<footer>
+
+</footer>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</body>
+</html>
