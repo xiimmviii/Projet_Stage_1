e3053c3e49e58a37e84371d23a4165c48c57872d
Marine <marinepelletier@me.com>
2019-09-19 14:42:46 +0200

    INDEX OP

--- index.php
+++ index.php
@@ -4,35 +4,125 @@ require('header.php');
 
 <main>
     <div class="row">
-        <div class="col-12 text-center index-img-titre p-4"><h1>EFFET DE TITRE</h1></div>
+        <div class="col-12 text-center index-img-titre p-4">
+            <h1>EFFET DE TITRE</h1>
+        </div>
     </div>
     <div class="row">
         <div class="col-lg-3 offset-lg-7 col-md-8 offset-md-2 mt-5  p-4 index-img-description"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto alias rem error ipsa iste temporibus id facilis illum optio dolor? <br>
-    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet dolores, eaque animi facilis, aliquid cupiditate numquam illo explicabo nam dolor perferendis ab odit deserunt totam illum fugit excepturi. Iure expedita neque hic, cum et nam assumenda culpa reprehenderit. Nemo, suscipit!</div>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet dolores, eaque animi facilis, aliquid cupiditate numquam illo explicabo nam dolor perferendis ab odit deserunt totam illum fugit excepturi. Iure expedita neque hic, cum et nam assumenda culpa reprehenderit. Nemo, suscipit!</div>
     </div>
     <div class="row">
         <div class="col-lg-3 offset-lg-1 col-md-8 offset-md-2 mt-5 mb-5  p-4 index-img-description"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto alias rem error ipsa iste temporibus id facilis illum optio dolor? <br>
-    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet dolores, eaque animi facilis, aliquid cupiditate numquam illo explicabo nam dolor perferendis ab odit deserunt totam illum fugit excepturi. Iure expedita neque hic, cum et nam assumenda culpa reprehenderit. Nemo, suscipit!</div>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet dolores, eaque animi facilis, aliquid cupiditate numquam illo explicabo nam dolor perferendis ab odit deserunt totam illum fugit excepturi. Iure expedita neque hic, cum et nam assumenda culpa reprehenderit. Nemo, suscipit!</div>
     </div>
 </main>
 
 <section>
-    <div class="row justify-content-around bg-secondary">
-    <a class="col-md-2 col-sm-12 btn indexliens m-2" href="#"> Spécialité 1 </a>
-    <a class="col-md-2 col-sm-12 btn indexliens m-2" href="#"> Spécialité 2 </a>
-    <a class="col-md-2 col-sm-12 btn indexliens m-2" href="#"> Spécialité 3 </a>
-    <a class="col-md-2 col-sm-12 btn indexliens m-2" href="#"> Spécialité 4 </a>
+    <div class="row justify-content-around bg-dark">
+        <a class="col-md-2 col-sm-12 btn indexliens m-4" href="#"> Spécialité 1 </a>
+        <a class="col-md-2 col-sm-12 btn indexliens m-4" href="#"> Spécialité 2 </a>
+        <a class="col-md-2 col-sm-12 btn indexliens m-4" href="#"> Spécialité 3 </a>
+        <a class="col-md-2 col-sm-12 btn indexliens m-4" href="#"> Spécialité 4 </a>
     </div>
 </section>
 
 <section>
     <div class="row presentation">
-<div class="col-8 mx-auto mt-5 mb-5 ">
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, corrupti animi quisquam nulla natus quasi eos accusamus, sapiente maiores maxime ratione corporis repellat. Aperiam saepe odio explicabo facilis aliquam et fuga impedit voluptate possimus. Illum excepturi libero, veritatis eligendi suscipit nihil explicabo perspiciatis optio nisi? Molestias nisi eum quam tempora.</p></div>
+        <div class="col-8 mx-auto mt-5 mb-5 ">
+            <div class="row">
+                <div class="col-md-8">
+                    <h1 class="m-3 text-primary"> Electricien, plombier, chauffagiste... sur la région /// </h1>
+                    <h3 class="mb-3"> Des interventions dans les communes de ---------
+                        24h/24, 7j/7
+                    </h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, corrupti animi quisquam nulla natus quasi eos accusamus, sapiente maiores maxime ratione corporis repellat. Aperiam saepe odio explicabo facilis aliquam et fuga impedit voluptate possimus. Illum excepturi libero, veritatis eligendi suscipit nihil explicabo perspiciatis optio nisi? Molestias nisi eum quam tempora.</p>
+                </div>
+                <div class="col-md-4 text-center"><img src="image/logo.jpg" class="img-fluid" />
+
+                    <a href="telto:+33000000000" class="btn text-light boutontel mt-3 "><i class="fas fa-phone-alt"></i> 00 00 00 00 00 </a></div>
+            </div>
+
+        </div>
     </div>
 </section>
 
 <section>
+    <div class="row bg-dark">
+        <div class="col-12 mt-3 mb-3">
+            <h3 class="text-center text-warning"> <i class="fas fa-star"></i> <i class="fas fa-star"></i> <i class="fas fa-star"></i> </h3>
+            <h2 class="text-center text-light">Compétence , Suivi, Engagements </h2>
+            <h3 class="text-warning text-center"> <i class="fas fa-star"></i> <i class="fas fa-star"></i> <i class="fas fa-star"></i> </h3>
+            <p class="col-md-6 col-sm-12 mx-auto text-light mt-4"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium nostrum quidem tempora. Eveniet dolor iste delectus doloremque, officiis nemo accusantium praesentium sequi alias quos libero odio quisquam quaerat, eligendi laborum, facere earum molestias temporibus. Exercitationem ipsa explicabo ex perspiciatis architecto facilis? Nulla magnam, modi ducimus laudantium ut inventore voluptatibus debitis. </p>
+        </div>
+    </div>
+
+</section>
+
+<section>
+    <div class="row presentation">
+        <div class="col-12 pb-4 ">
+            <h1 class="text-center mt-4 mb-4"> Partenaires </h1>
+            <h3 class="text-center text-primary"> <i class="fas fa-handshake"></i> <i class="fas fa-handshake"></i> <i class="fas fa-handshake"></i> </h3>
+            <div class="row justify-content-around">
+                <div class="col-md-3 col-sm-10"><img src="image/logo.jpg" class="img-fluid" /></div>
+                <div class="col-md-3 col-sm-10"><img src="image/logo.jpg" class="img-fluid" /></div>
+                <div class="col-md-3 col-sm-10"><img src="image/logo.jpg" class="img-fluid" /></div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section>
+    <div class="row">
+        <div class="col-12 bottomdevis">
+            <h2 class="text-center mt-3 mb-3"> Votre artisan ............. </h2>
+            <div class="row justify-content-center">
+                <div class="col-md-3 text-center mt-3 mb-3">
+                    <a href="telto:+33000000000" class="bg-primary btn btnrond text-light"><i class="fas fa-phone-alt"></i></a>
+                    <br>
+                    <a href="telto:+33000000000" class="bg-primary btn text-light mt-4">00 00 00 00 00 </a>
+                </div>
+                <div class="col-md-3 text-center mt-3 mb-3">
+                    <a href="mailto:mail@mail.com" class="bg-primary btn btnrond text-light"><i class="far fa-envelope"></i></a>
+                    <br>
+                    <a href="mailto:mail@mail.com" class="bg-primary btn text-light mt-4"> Contact </a>
+                </div>
+                <div class="col-md-3 text-center mt-3 mb-3">
+                    <a href="#" class="bg-primary btn btnrond text-light"><i class="fas fa-euro-sign"></i></a>
+                    <br>
+                    <a href="#" class="bg-primary btn text-light mt-4"> Devis </a>
+                </div>
+            </div>
+            <h4 class="col-8 mt-3 mx-auto"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti hic accusamus fuga asperiores quod magnam esse recusandae vero animi laudantium.</h4>
+            <hr>
+            <div class="row justify-content-center mb-4">
+                <div class="col-md-2 col-sm-10 localisation">
+                    <i class="fas fa-map-pin"></i> - LOCALISATION
+                    <br>
+                    <i class="fas fa-map-pin"></i> - LOCALISATION 
+                </div>
+                <div class="col-md-2 col-sm-10 localisation">
+                    <i class="fas fa-map-pin"></i> - LOCALISATION
+                    <br>
+                    <i class="fas fa-map-pin"></i> - LOCALISATION 
+                </div>
+                <div class="col-md-2 col-sm-10 localisation">
+                    <i class="fas fa-map-pin"></i> - LOCALISATION
+                    <br>
+                    <i class="fas fa-map-pin"></i> - LOCALISATION 
+                </div>
+                <div class="col-md-2 col-sm-10 localisation">
+                    <i class="fas fa-map-pin"></i> - LOCALISATION
+                    <br>
+                    <i class="fas fa-map-pin"></i> - LOCALISATION 
+                </div>
+
+            </div>
+        </div>
+    </div>
+
 </section>
 
 <?php
