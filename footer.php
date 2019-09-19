7499f27064ad18032ccc21cc57c3964a9b47dc2b
Marine <marinepelletier@me.com>
2019-09-19 10:29:24 +0200

    CSS Update + Footer

--- footer.php
+++ footer.php
@@ -1,16 +1,25 @@
-<footer>
-    <div class="row">
-<div class="col-md-3 offset-md-2"> 
-    <div class="footerh3"> Nom de la société </div>
-    <div class="footeradresse"> Adresse ligne 1 <br> Adresse ligne 2 </div>
-    <div class="footercontact"> Téléphone : <span class="footertel"><a href="tel:+33000000000"> 00.00.00.00.00</a> </span></div>
-</div>
-
-</div> 
+<footer class="bg-info">
+    <div class="row m-3 mb-4 p-1">
+        <div class="col-md-4 col-sm-10 offset-md-2">
+            <div class="footeradresse"> Adresse ligne 1 <br> Adresse ligne 2 </div>
+            <div class="footercontact"> Téléphone : <span class="footertel"><a href="tel:+33000000000"> 00.00.00.00.00</a> </span></div>
+            <div class="footercontact"> Mail : <span class="footermail"><a href="mailto:mail@mail.com"> mail@mail.com</a> </span></div>
+        </div>
+        <div class="col-md-4 col-sm-10 text-center">
+            <div class="footerh3 pb-3"> Laissez votre témoignage sur Google </div>
+            <a href="pagegoogle">
+                <div class="col-10 btn boutonavis"> <i class="fas fa-star"></i> Laissez votre avis ! </div>
+            </a>
+        </div>
+    </div>
+    <div class="row bg-secondary text-light p-3">
+        <div class="col-12 copyright"> Copyright © 2019 SOCIETE | <a href="mentionslegales" class="copyright-a">Mentions légales</a> | <a href="CGU" class="copyright-a">CGU</a></div>
+    </div>
 
 </footer>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.slim.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.15.0/umd/popper.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </body>
+
 </html>
