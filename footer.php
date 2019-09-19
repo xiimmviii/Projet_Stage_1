49e444c7b85a0491d44c73ea37850f0280d0dc41
Marine <marinepelletier@me.com>
2019-09-19 10:52:53 +0200

    Good Update ?

--- footer.php
+++ footer.php
@@ -1,20 +1,20 @@
 <footer>
-    <div class="row p-3">
-        <div class="col-md-4 col-sm-10 offset-md-2">
+    <div class="row justify-content-center">
+        <div class="col-md-4 col-sm-12 m-3">
             <div class="footerh3 pb-3"> Nom de la société </div>
             <div class="footeradresse"> Adresse ligne 1 <br> Adresse ligne 2 </div>
             <div class="footercontact"> Téléphone : <a href="tel:+33000000000" class="footertel"> 00.00.00.00.00</a> </div>
             <div class="footercontact"> Mail : <a href="mailto:mail@mail.com" class="footermail"> mail@mail.com</a> </div>
         </div>
-        <div class="col-md-4 col-sm-10 text-center">
+        <div class="col-md-4 col-sm-12 text-center m-3">
             <div class="footerh3 pb-3"> Laissez votre témoignage sur Google </div>
             <a href="pagegoogle">
                 <div class="col-10 btn boutonavis"> <i class="fas fa-star"></i> Laissez votre avis ! </div>
             </a>
         </div>
     </div>
-    <div class="row copyrow text-light p-3">
-        <div class="col-12 copyright"> Copyright © 2019 SOCIETE | <a href="mentionslegales" class="copyright-a">Mentions légales</a> | <a href="CGU" class="copyright-a">CGU</a></div>
+    <div class="row copyrow text-light">
+        <div class="col-12 copyright m-3"> Copyright © 2019 SOCIETE | <a href="mentionslegales" class="copyright-a">Mentions légales</a> | <a href="CGU" class="copyright-a">CGU</a></div>
     </div>
 
 </footer>
